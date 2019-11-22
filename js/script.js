76677b38d09ceaae0e239185aa6fbae062b8fd7b
Michael Kuczynski <mkuczynski@outlook.com>
2019-11-21 20:53:08 -0600

    Changed html and js files
    Added name and email to html, added eventlistener to check for a click on the button which will call the btnClick function that will give the alert and change the textoutput

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+ document.getElementById("entrybutton").addEventListener("click", btnClick);
+}
 
-
-
-
-
-
+function btnClick(){
+	alert("Michael Kuczynski: " + document.getElementById("entryinput").value);
+	document.getElementById("textoutput").innerHTML = document.getElementById("entryinput").value;
+}
 
 window.addEventListener('load', init);
